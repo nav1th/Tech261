2df954c3be7ec51ebc836bf562b256fcb074ce75
nav1th <navith@keemail.me>
2024-06-17 14:55:57 +0100

    an actual working program yay

---
+++ java-basics/src/main/java/com/sparta/jk/Program.java
@@ -0,0 +1,26 @@
+package com.sparta.jk;
+
+public class Program {
+    public static void main(String[] args)
+    {
+        int timeOfDay = 21;
+        System.out.println(getGreeting(timeOfDay));
+    }
+
+    private static String getGreeting(int timeOfDay) {
+        String greeting;
+        if (timeOfDay >= 5 && timeOfDay <= 12)
+        {
+            greeting = "Good morning!";
+        }
+        else if (timeOfDay >= 12 && timeOfDay <= 18)
+        {
+            greeting = "Good afternoon!";
+        }
+        else
+        {
+            greeting ="Good evening!";
+        }
+        return greeting;
+    }
+}
