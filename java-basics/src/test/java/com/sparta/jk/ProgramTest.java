a0a7ebffe012b52f70341318c90fd86513b41cde
nav1th <navith@keemail.me>
2024-06-17 15:07:07 +0100

    added junit testing system

---
+++ java-basics/src/test/java/com/sparta/jk/ProgramTest.java
@@ -0,0 +1,10 @@
+package com.sparta.jk;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ProgramTest {
+    @Test
+    public void basicTest(){
+        Assertions.assertTrue(5==6);
+    }
+}
