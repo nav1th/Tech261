2df7bc8afb4c0c4a2641481f5d53b852e7e9b7c6
nav1th <navith@keemail.me>
2024-06-18 09:31:57 +0100

    added error handling WHEN user inputs invalid time

--- java-basics/src/main/java/com/sparta/jk/Program.java
+++ java-basics/src/main/java/com/sparta/jk/Program.java
@@ -7,20 +7,29 @@ public class Program {
         System.out.println(getGreeting(timeOfDay));
     }
 
-    private static String getGreeting(int timeOfDay) {
+    public static String getGreeting(int timeOfDay) {
+        if(timeOfDay >= 24){
+            throw new IllegalArgumentException("Time of day can't be more than 23");
+        }
+        else if(timeOfDay < 0){
+            throw new IllegalArgumentException("Time of day can't be a negative number");
+        }
+
         String greeting;
-        if (timeOfDay >= 5 && timeOfDay <= 12)
+
+        if (timeOfDay >= 5 && timeOfDay <= 11)   // 05 - 11
         {
             greeting = "Good morning!";
         }
-        else if (timeOfDay >= 12 && timeOfDay <= 18)
+        else if (timeOfDay >= 12 && timeOfDay <= 18) // 12 - 18
         {
             greeting = "Good afternoon!";
         }
-        else
+        else  // 19 - 04
         {
             greeting ="Good evening!";
         }
+
         return greeting;
     }
 }
--- java-basics/src/test/java/com/sparta/jk/ProgramTest.java
+++ java-basics/src/test/java/com/sparta/jk/ProgramTest.java
@@ -1,10 +1,50 @@
 package com.sparta.jk;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ProgramTest {
     @Test
-    public void basicTest(){
-        Assertions.assertTrue(5==6);
+    @DisplayName("GIVEN a time of 11, THEN greeting should return 'Good morning!'")
+    void check11ReturnGoodMorning(){
+        int timeOfDay = 11; 
+        String expectedGreeting = "Good morning!";
+        String actualGreeting = Program.getGreeting(timeOfDay);
+        Assertions.assertEquals(expectedGreeting, actualGreeting);
+    }
+    @Test
+    @DisplayName("GIVEN a time of 13, THEN greeting should return 'Good afternoon!'")
+    void check13ReturnsGoodAfternoon(){
+        int timeOfDay = 13;
+        String expectedGreeting = "Good afternoon!";
+        String actualGreeting = Program.getGreeting(timeOfDay);
+        Assertions.assertEquals(expectedGreeting, actualGreeting);
+    }
+    @Test
+    @DisplayName("GIVEN a time of 21, THEN greeting should return 'Good evening!'")
+    void check21ReturnsGoodEvening(){
+        int timeOfDay = 21; 
+        String expectedGreeting = "Good evening!";
+        String actualGreeting = Program.getGreeting(timeOfDay);
+        Assertions.assertEquals(expectedGreeting, actualGreeting);
+    }
+    @Test
+    @DisplayName("GIVEN a time of over 23, THEN a error should be returned with the message 'Time of day can't be more than 23'")
+    void checkOver23ReturnsError(){
+        int timeOfDay = 24;
+        String expectedErrorMessage = "Time of day can't be more than 23";
+        String actualErrorMessage = Assertions.assertThrows(IllegalArgumentException.class, () -> Program.getGreeting(timeOfDay)).getMessage();
+        Assertions.assertEquals(expectedErrorMessage, actualErrorMessage);
+
+    }
+
+    @Test
+    @DisplayName("GIVEN a time of under 0, THEN a error should be returned with the message 'Time of day can't be a negative number'")
+    void checkUnder0ReturnsError(){
+        int timeOfDay = -1;
+        String expectedErrorMessage = "Time of day can't be a negative number";
+        String actualErrorMessage = Assertions.assertThrows(IllegalArgumentException.class, () -> Program.getGreeting(timeOfDay)).getMessage();
+        Assertions.assertEquals(expectedErrorMessage, actualErrorMessage);
+
     }
 }
